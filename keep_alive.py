b37416d49470990a6924d285347c148a006368d8
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2021-04-04 13:42:16 +0900

    flaskのログレベルを下げる

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,12 @@
 # file name is keep_alive.py
 from flask import Flask
 from threading import Thread
+import logging
+# ロガーの取得
+werkzeug_logger = logging.getLogger("werkzeug")
+# レベルの変更
+werkzeug_logger.setLevel(logging.ERROR)
+
 app = Flask('')
 @app.route('/')
 def main():
